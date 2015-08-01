9fa8970fd04ff8d67bb00b0f1d06d5bde1e9b64a
yramocan <yramocan@umich.edu>
2015-08-01 02:02:18 -0400

    Finish user signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
