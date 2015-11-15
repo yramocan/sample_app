d4da82e92b8868b65f98f79a7ff0fc892f8d38b5
yramocan <yramocan@umich.edu>
2015-11-15 15:13:37 -0500

    prod env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  prod_host = "lit-shore-7122.herokuapp.com"
+  prod_host = 'https://lit-shore-7122.herokuapp.com'
   config.action_mailer.default_url_options = { :host => prod_host }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
