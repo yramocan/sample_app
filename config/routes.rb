43efe5467850cc022d33ca67b365e5a8ac46948b
yramocan <yramocan@umich.edu>
2015-11-14 23:49:12 -0500

    Add password reset & email config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'users#new'
   
   # Static Pages Routes
@@ -18,4 +22,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
