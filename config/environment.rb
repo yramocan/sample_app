ae6ea9b37f5d1f469f7d4869b3a1ab9ec18c7720
yramocan <yramocan@umich.edu>
2015-11-16 00:09:37 -0500

    Config ActionMailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -14,15 +14,3 @@ Rails.application.initialize!
 #   :authentication => :plain,
 #   :enable_starttls_auto => true
 # }
-
-config.action_mailer.delivery_method = :smtp
-
-# Configure ActionMailer to use MailGun
-config.action_mailer.smtp_settings = {
-	:port           => ENV['MAILGUN_SMTP_PORT'],
-	:address        => ENV['MAILGUN_SMTP_SERVER'],
-	:user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-	:password       => ENV['MAILGUN_SMTP_PASSWORD'],
-	:domain         => 'lit-shore-7122.herokuapp.com',
-	:authentication => :plain,
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  prod_host = 'https://lit-shore-7122.herokuapp.com'
+  prod_host = 'lit-shore-7122.herokuapp.com'
   config.action_mailer.default_url_options = { :host => prod_host }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
@@ -82,4 +82,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+# Configure ActionMailer to use MailGun
+config.action_mailer.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'lit-shore-7122.herokuapp.com',
+  :authentication => :plain,
+}
 end
