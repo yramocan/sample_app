829797ae3fffd36466d3fd35aaedd64cce51c6b3
yramocan <yramocan@umich.edu>
2015-11-27 19:52:18 -0500

    Finish microposts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
