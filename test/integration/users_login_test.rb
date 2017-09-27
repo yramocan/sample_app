80773d87b439b8ccdd3b5c81d8b449ad0e7037b1
Yuri Ramocan <yramocan@umich.edu>
2017-09-27 18:35:02 -0400

    Modify tests to remove deprecation warnings

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,7 +4,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, session: { email: "", password: "" }
+    post login_path, params: { session: { email: "", password: "" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
