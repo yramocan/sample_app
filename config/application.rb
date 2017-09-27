26cdef91dcdfc4fecfd21fa3b422069f9f16f119
Yuri Ramocan <yramocan@umich.edu>
2017-09-27 19:14:14 -0400

    Remove transactional callbacks from application.rb because it has been removed

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local
 
